4f4b93656fa7a0fd5a84f52c77849144c6d0127a
Mahak Bharatkar <mahakbharatkar@Mahaks-MacBook-Air.local>
2024-01-14 03:13:16 +0530

    modal basic

--- src/components/Habits/index.js
+++ src/components/Habits/index.js
@@ -1,27 +1,47 @@
-import { LinearProgress, Typography } from "@mui/material";
+import { Button, Fab, LinearProgress, Modal, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import styles from "./styles.module.css";
 import ListItem from "./components/ListItem";
 import { useEffect, useState } from "react";
 const dummyHabits = [
-  {id:1, title: "Drink Water", description: "Drink 8 glasses of water everyday", completed: false },
-  {id:2, title: "Code", description: "Code 1 problem everyday", completed: false },
-  {id:3, title: "Journal", description: "Write 2 pages in your journal", completed: false },
-  {id:4, title: "Excersice ", description: "Do 7 Suryanamaskars", completed: false },
+  {
+    id: 1,
+    title: "Drink Water",
+    description: "Drink 8 glasses of water everyday",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Code",
+    description: "Code 1 problem everyday",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Journal",
+    description: "Write 2 pages in your journal",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Excersice ",
+    description: "Do 7 Suryanamaskars",
+    completed: false,
+  },
 ];
 
 const Habits = () => {
-    const [habits, setHabits] = useState([]);
+  const [habits, setHabits] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
-    const totalHabits = habits.length;
-    const habitsCompleted = habits.filter((habit) => habit.completed).length;
+  const totalHabits = habits.length;
+  const habitsCompleted = habits.filter((habit) => habit.completed).length;
 
-    
-    const progress = Math.ceil((habitsCompleted/totalHabits)*100);
+  const progress = Math.ceil((habitsCompleted / totalHabits) * 100);
 
-
-    useEffect(()=>{
-        setHabits(dummyHabits);
-    },[]);
+  useEffect(() => {
+    setHabits(dummyHabits);
+  }, []);
 
   return (
     <div className={styles.habits_container}>
@@ -54,6 +74,33 @@ const Habits = () => {
       {habits.map((item) => {
         return <ListItem key={item.id} item={item} setHabits={setHabits} />;
       })}
+
+      <Fab
+        aria-label="add"
+        color="secondary"
+        onClick={() => setShowModal(true)}
+      >
+        <AddIcon />
+      </Fab>
+
+      <Modal
+        open={showModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className={styles.modal_container}>
+          <span>Add New Habit</span>
+
+          {/* Todo: When onClick of submit of the the add habit. give id by uuid and title, 
+          description as entered. But always mark the completed as false initially */}
+
+          <div>
+            <Button variant="outlined" onClick={() => setShowModal(false)}>
+              Close
+            </Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
